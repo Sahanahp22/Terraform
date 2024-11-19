cea3c9d843952185388fbb1d9a978ef099904679
Sahanahp22 <sahanahp56@gmail.com>
2024-11-19 07:42:46 +0000

    Vpc

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region     = "eu-south-1"
-  access_key = "AKIAYHJANAN7JTINU5HC"    
-  secret_key = "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" 
+   
 }
 
 resource "aws_vpc" "main" {
